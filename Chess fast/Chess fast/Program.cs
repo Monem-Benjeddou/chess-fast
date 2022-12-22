92066039ffa1001b9415bebcddd2bcfcb7fb06c3
Monem Benjeddou <monem1bouazizi@gmail.com>
2022-12-22 14:05:54 +0100

    Addingvisualeffect

--- Chess fast/Chess fast/Program.cs
+++ Chess fast/Chess fast/Program.cs
@@ -1,7 +1,13 @@
 ﻿using Chess_fast;
+<<<<<<< HEAD
 int N = 11;
 int[] knightPos = { 0, 0 };
 int[] targetPos = { 9, 10 };
+=======
+int N =20;
+int[] knightPos = { 0, 0 };
+int[] targetPos = { 10, 9 };
+>>>>>>> b7a092c3c68f21ffbd3d00294378d41303934b7b
 
 // Function call
 Console.WriteLine(Prog.minStepToReachTarget(knightPos, targetPos, N));
